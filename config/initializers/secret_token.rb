241aa034de4c17c18d0efd185450e6957ecb7622
ben wanicur <bwanicur@gmail.com>
2011-11-01 21:20:30 -0700

    Trying Refinery CMS for the SecondChance project.  Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondChance::Application.config.secret_token = '3581db829046d6bf30a922a7778eeb381ed8ece11a529fb458892e8a8ec869c222f0a9dc01f8b315797f4c41877a7cc6c394b49891e1e2ed8b04f73cea2562c7'
