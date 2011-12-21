c885302fe8e33cf996b0220acacf273aeb147075
David Lundy <dlundy@gmail.com>
2011-12-21 07:43:42 +0000

    Fix escaped entities in dog content -Unescape html entities when pulling from rescuegroups.org -Use raw on breed

--- lib/jobs/update_sc_db_job.rb
+++ lib/jobs/update_sc_db_job.rb
@@ -18,30 +18,32 @@ class UpdateScDbJob < Struct.new(:limit)
       end
     end
   end
-  
+
   protected
-    
+
   def massage_data(dog)
     dog['name'] = dog['name'].downcase # let's store our dog names in lowercase
+    dog['breed'] = CGI.unescapeHTML(dog['breed'])
+    dog['description'] = CGI.unescapeHTML(dog['description'])
     dog
   end
-  
+
   def get_photo_data(dog_data)
     dog_data.select{|k,v| k.to_s =~ /pic/}
   end
-  
+
   def set_photos!(dog, photo_data)
     return unless dog.dog_photos.empty?
     photo_data.each do |key, value|
       size = (key.to_s =~ /tmn/ ? 'thumb' : 'regular')
       dog.dog_photos << DogPhoto.new({:rescue_groups_key => key.to_s, :url => value, :size => size})
     end
-  end  
-  
+  end
+
   def update_photos!(dog, photo_data)
     photo_data.each do |key, value|
       dog.dog_photos.where(:rescue_groups_key => key.to_s).first.update_attribute(:url, value)
     end
   end
-  
+
 end
