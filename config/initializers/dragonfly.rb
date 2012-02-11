4cbf87df54ddd3c042515f642b74f4bea88e25f7
ben wanicur <bwanicur@gmail.com>
2012-02-10 19:11:49 -0800

    shoot.  keep forgetting config stuff for heroku

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,9 +1,10 @@
 # http://markevans.github.com/dragonfly/file.Rails3.html
 
-require 'dragonfly'
-
-app = Dragonfly[:images]
-app.configure_with(:imagemagick)
-app.configure_with(:rails)
-
-app.define_macro(ActiveRecord::Base, :image_accessor)
+# NOTE: hoping this is only needed for local rails development.  RefineryCMS should handle dragonfly / heroku config
+unless Rails.env.production?
+  require 'dragonfly'
+  app = Dragonfly[:images]
+  app.configure_with(:imagemagick)
+  app.configure_with(:rails)
+  app.define_macro(ActiveRecord::Base, :image_accessor)
+end
