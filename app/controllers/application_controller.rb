c12cd60ea83f65e349ef98352ae356d77a4d03b6
ben wanicur <bwanicur@gmail.com>
2012-02-12 18:18:25 -0800

    trying to include the config in the initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  ActionMailer::Base.default_url_options[:host] = ENV['SC_HOSTNAME']
-  
   protected
   
   def dpage
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,3 +2,5 @@ DOG_APPLICATION_FORM_URL = 'http://www.formspring.com/forms/?886457-oDRwG4qugN'
 DOG_IMPORT_LIMIT = 300
 
 HOSTNAME = ENV['SC_HOSTNAME']
+
+ActionMailer::Base.default_url_options[:host] = ENV['SC_HOSTNAME']
