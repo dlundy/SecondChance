f026cee0294102f6fb63909da29c2581d533855f
ben wanicur <bwanicur@gmail.com>
2012-02-12 18:09:36 -0800

    not sure why this should fix the problem.  perhaps the devise library (consumed by refinery) is not picking up the config set in the last commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  ActionMailer::Base.default_url_options[:host] = ENV['SC_HOSTNAME']
+  
   protected
   
   def dpage
