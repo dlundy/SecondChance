37d6d003cc39b3d1625e0f6f92d15ea84e426803
ben wanicur <bwanicur@gmail.com>
2012-02-12 18:48:21 -0800

    production mode is send email and revealing bugs

--- vendor/engines/members/app/models/member.rb
+++ vendor/engines/members/app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable, :recoverable
   
-  acts_as_indexed :fields => [:email, :first_name, :last_name, :address_1, :address_2, :city, :state, :phone, :description]
+  acts_as_indexed :fields => [:email, :first_name, :last_name, :address_1, :address_2, :city, :state, :phone]
 
   validates :email, :presence => true, :uniqueness => true
   
