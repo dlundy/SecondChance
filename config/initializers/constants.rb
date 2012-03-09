1853fea16ab8b637c3f76c32ecb0fa6c3fb2c36b
David Lundy <dlundy@gmail.com>
2012-03-08 18:44:19 -0800

    Add Volunteer button to home page

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,7 @@
 # DOG_APPLICATION_FORM_URL = 'http://www.formspring.com/forms/?886457-oDRwG4qugN'
 DOG_APPLICATION_FORM_URL = 'http://www.formstack.com/forms/?886457-oDRwG4qugN'
 DOG_FOSTER_FORM_URL = 'http://www.formstack.com/forms/?1062974-rJm7TK6vXu'
+VOLUNTEER_FORM_URL = 'http://www.formstack.com/forms/?1023577-EDjVt7W9l5'
 DOG_IMPORT_LIMIT = 300
 
 HOSTNAME = ENV['SC_HOSTNAME']
